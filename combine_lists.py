210d394c652027773c929bcf3f6f502e2d4c9891
Ravinder Deol <57337436+ravinderdeol@users.noreply.github.com>
2022-02-11 12:30:27 +0100

    create combine lists

---
+++ combine_lists.py
@@ -0,0 +1,17 @@
+# plus operator
+
+list1 = [1, 2, 3]
+list2 = [4, 5, 6]
+
+list3 = list1 + list2
+
+print(list3) # prints [1, 2, 3, 4, 5, 6]
+
+# extend method
+
+list1 = [1, 2, 3]
+list2 = [4, 5, 6]
+
+list1.extend(list2)
+
+print(list1) # prints [1, 2, 3, 4, 5, 6]
